c72998b81cca67c0721a49623a42835be1caf971
DicksonOndaraNyabochwa <yungondara@gmail.com>
2025-03-18 09:27:18 +0300

    complete events listeners

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 function addingEventListener() {
+  const input = document.getElementById('button');
+
+  if (input) {
+    input.addEventListener('click', function () {
+      console.log('Button clicked!');
+    });
+  } else {
+    console.error("Element with ID 'button' not found!");
+  }
 }
+
+// Call the function to bind the event listener
+addingEventListener();
